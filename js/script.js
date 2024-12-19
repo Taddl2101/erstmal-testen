99b9763562d256cb93fa7877c1a6d1b8916d958e
iveltier <iveltier@mailbox.org>
2024-12-19 21:41:26 +0100

    big baba bubu update js file sehr gekürzt mit universeller function, nur noch gewünschtes element + elementactive Status + boxShadowFarbe und dann geht + alle function geht jetzt auch darüber

--- js/script.js
+++ js/script.js
@@ -23,162 +23,27 @@ const states = [
   "actinoideActive",
 ];
 
-// EdelgaseBtn
+// alle zustände auf false setzen
 
 let edelgaseActive = false;
-
-function changeEdelgaseColor() { 
-  edelgaseActive = !edelgaseActive;
-
-  edelgaseElements.forEach((element) => {
-    if (edelgaseActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(247, 144, 187), 0 0 5px 1px rgb(247, 144, 187)";
-      element.classList.add("highlight");
-    } else {
-      element.style.boxShadow = "";
-      element.classList.remove("highlight");
-    }
-  });
-}
-
-// Halogene
-
 let halogeneActive = false;
-
-function changeHalogeneColor() {
-  halogeneActive = !halogeneActive;
-
-  halogeneElements.forEach((element) => {
-    if (halogeneActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(253, 76, 106), 0 0 5px 1px rgb(253, 76, 106)";
-      element.classList.add("highlight");
-    } else {
-      element.style.boxShadow = "";
-      element.classList.remove("highlight");
-    }
-  });
-}
-// AlkalimetalleBtn
-
 let alkalimetalleActive = false;
-
-function changeAlkalimetalleColor() {
-  alkalimetalleActive = !alkalimetalleActive;
-
-  alkalimetalleElements.forEach((element) => {
-    if (alkalimetalleActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(42, 233, 99), 0 0 5px 1px rgb(42, 233, 99)";
-      element.classList.add("highlight");
-    } else {
-      element.style.boxShadow = "";
-      element.classList.remove("highlight");
-    }
-  });
-}
-// ErdalkalimetalleBtn
-
 let erdalkalimetalleActive = false;
-
-function changeErdalkalimetalleColor() {
-  erdalkalimetalleActive = !erdalkalimetalleActive;
-
-  erdalkalimetalleElements.forEach((element) => {
-    if (erdalkalimetalleActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(10, 172, 24), 0 0 5px 1px rgb(10, 172, 24)";
-      element.classList.add("highlight");
-    } else {
-      element.style.boxShadow = "";
-      element.classList.remove("highlight");
-    }
-  });
-}
-// ÜbergangsmetalleBtn
-
 let übergangsmetalleActive = false;
-
-function changeÜbergangsmetalleColor() {
-  übergangsmetalleActive = !übergangsmetalleActive;
-
-  übergangsmetalleElements.forEach((element) => {
-    if (übergangsmetalleActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(0, 50, 187), 0 0 5px 1px rgb(0, 50, 187)";
-      element.classList.add("highlight");
-    } else {
-      element.style.boxShadow = "";
-      element.classList.remove("highlight");
-    }
-  });
-}
-// MetalleBtn
-
 let metalleActive = false;
-
-function changeMetalleColor() {
-  metalleActive = !metalleActive;
-
-  metalleElements.forEach((element) => {
-    if (metalleActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 3px rgb(95, 0, 95), 0 0 5px 1px rgb(95, 0, 95)";
-      element.classList.add("highlight");
-    } else {
-      element.style.boxShadow = "";
-      element.classList.remove("highlight");
-    }
-  });
-}
-// HalbmetalleBtn
-
 let halbmetalleActive = false;
-
-function changeHalbmetalleColor() {
-  halbmetalleActive = !halbmetalleActive;
-
-  halbmetalleElements.forEach((element) => {
-    if (halbmetalleActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(168, 11, 182), 0 0 5px 1px rgb(168, 11, 182)";
-      element.classList.add("highlight");
-    } else {
-      element.style.boxShadow = "";
-      element.classList.remove("highlight");
-    }
-  });
-}
-// NichtmetalleBtn
-
 let nichtmetalleActive = false;
-
-function changeNichtmetalleColor() {
-  nichtmetalleActive = !nichtmetalleActive;
-
-  nichtmetalleElements.forEach((element) => {
-    if (nichtmetalleActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(207, 15, 15), 0 0 5px 1px rgb(207, 15, 15)";
-      element.classList.add("highlight");
-    } else {
-      element.style.boxShadow = "";
-      element.classList.remove("highlight");
-    }
-  });
-}
-// LanthanoideBtn
-
 let lanthanoideActive = false;
+let actinoideActive = false;
 
-function changeLanthanoideColor() {
-  lanthanoideActive = !lanthanoideActive;
+// universelle function zum change color
 
-  lanthanoideElements.forEach((element) => {
-    if (lanthanoideActive) {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(0, 99, 156), 0 0 5px 1px rgb(0, 99, 156)";
+function changeColor(state, elements, boxShadow) {
+  window[state] = !window[state];
+
+  elements.forEach((element) => {
+    if (window[state]) {
+      element.style.boxShadow = boxShadow;
       element.classList.add("highlight");
     } else {
       element.style.boxShadow = "";
@@ -186,9 +51,6 @@ function changeLanthanoideColor() {
     }
   });
 }
-// ActinoideBtn
-
-let actinoideActive = false;
 
 function changeActinoideColor() {
   actinoideActive = !actinoideActive;
@@ -207,7 +69,8 @@ function changeActinoideColor() {
 
 //AlleBtn
 
-// const aktive = ; const muss einem festen wert zugewiesen werden => deswegen ging gestern der code nicht 
+// const aktive = ; const muss einem festen wert zugewiesen werden => deswegen ging gestern der code nicht
+
 function changeAlleColor() {
   alleElemente.forEach((element) => {
     element.style.boxShadow = "";
@@ -215,56 +78,56 @@ function changeAlleColor() {
   });
 
   setTimeout(() => {
-    alkalimetalleElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(42, 233, 99), 0 0 5px 1px rgb(42, 233, 99)";
-      element.classList.add("highlight");
-    });
-    erdalkalimetalleElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(10, 172, 24), 0 0 5px 1px rgb(10, 172, 24)";
-      element.classList.add("highlight");
-    });
-    übergangsmetalleElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(0, 50, 187), 0 0 5px 1px rgb(0, 50, 187)";
-      element.classList.add("highlight");
-    });
-    metalleElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 3px rgb(95, 0, 95), 0 0 5px 1px rgb(95, 0, 95)";
-      element.classList.add("highlight");
-    });
-    halbmetalleElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(168, 11, 182), 0 0 5px 1px rgb(168, 11, 182)";
-      element.classList.add("highlight");
-    });
-    nichtmetalleElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(207, 15, 15), 0 0 5px 1px rgb(207, 15, 15)";
-      element.classList.add("highlight");
-    });
-    halogeneElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(247, 144, 187), 0 0 5px 1px rgb(247, 144, 187)";
-      element.classList.add("highlight");
-    });
-    edelgaseElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(253, 76, 106), 0 0 5px 1px rgb(253, 76, 106)";
-      element.classList.add("highlight");
-    });
-    actinoideElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(117, 190, 233), 0 0 5px 1px rgb(117, 190, 233)";
-      element.classList.add("highlight");
-    });
-    lanthanoideElements.forEach((element) => {
-      element.style.boxShadow =
-        "inset 0 0 8px 1px rgb(0, 99, 156), 0 0 5px 1px rgb(0, 99, 156)";
-      element.classList.add("highlight");
-    });
+    changeColor(
+      "alkalimetalleActive",
+      alkalimetalleElements,
+      "inset 0 0 8px 1px rgb(42, 233, 99), 0 0 5px 1px rgb(42, 233, 99)"
+    );
+    changeColor(
+      "erdalkalimetalleActive",
+      erdalkalimetalleElements,
+      "inset 0 0 8px 1px rgb(10, 172, 24), 0 0 5px 1px rgb(10, 172, 24)"
+    );
+    changeColor(
+      "übergangsmetalleActive",
+      übergangsmetalleElements,
+      "inset 0 0 8px 1px rgb(0, 50, 187), 0 0 5px 1px rgb(0, 50, 187)"
+    );
+    changeColor(
+      "metalleActive",
+      metalleElements,
+      "inset 0 0 8px 3px rgb(95, 0, 95), 0 0 5px 1px rgb(95, 0, 95)"
+    );
+    changeColor(
+      "halbmetalleActive",
+      halbmetalleElements,
+      "inset 0 0 8px 1px rgb(168, 11, 182), 0 0 5px 1px rgb(168, 11, 182)"
+    );
+    changeColor(
+      "nichtmetalleActive",
+      nichtmetalleElements,
+      "inset 0 0 8px 1px rgb(207, 15, 15), 0 0 5px 1px rgb(207, 15, 15)"
+    );
+    changeColor(
+      "halogeneActive",
+      halogeneElements,
+      "inset 0 0 8px 1px rgb(247, 144, 187), 0 0 5px 1px rgb(247, 144, 187)"
+    );
+    changeColor(
+      "edelgaseActive",
+      edelgaseElements,
+      "inset 0 0 8px 1px rgb(253, 76, 106), 0 0 5px 1px rgb(253, 76, 106)"
+    );
+    changeColor(
+      "actinoideActive",
+      actinoideElements,
+      "inset 0 0 8px 1px rgb(117, 190, 233), 0 0 5px 1px rgb(117, 190, 233)"
+    );
+    changeColor(
+      "lanthanoideActive",
+      lanthanoideElements,
+      "inset 0 0 8px 1px rgb(0, 99, 156), 0 0 5px 1px rgb(0, 99, 156)"
+    );
   }, 300);
 
   // aus array auf jeden einzeln zustände zugreifen
